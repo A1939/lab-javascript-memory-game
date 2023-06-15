2ad9b10a27fea8636d3b03b40e18cc09116fa8d3
Ale Tsai <alejandrot.1939@gmail.com>
2023-06-15 21:04:10 +0100

    Iteration 1 & 2 (2 Jasmine error on Iteration 2.2)

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,38 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
-  shuffleCards() {
-    // ... write your code here
+  shuffleCards(cardsArray) {
+    if (!cardsArray) {
+      return undefined
+    }
+    const mixedCards = []
+    for (let i = cardsArray.length; i > 0; i--) {
+      let j = Math.floor(Math.random() * i);
+      let a = cardsArray.splice(j,1)
+      a = a[0]
+      mixedCards.push(a)
+    }
+    return mixedCards
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    }
+    return false;
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === 12) {
+      return true;
+    }
+    return false;
   }
 }
